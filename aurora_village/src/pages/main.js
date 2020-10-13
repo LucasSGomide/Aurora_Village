948424714f0cf47968a3312ec0a91eec3afe7bd9
Lucas Costa Ventura <lucascv333@gmail.com>
2020-10-13 19:51:31 -0300

    Adicionando mapa

--- aurora_village/src/pages/main.js
+++ aurora_village/src/pages/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
+import Mapa from '../components/Map';
 
 class Main extends React.Component {
   render() {
     return (
       <main>
         <Header />
+        <Mapa />
       </main>
     );
   }
