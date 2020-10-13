fe0adc40c563e44a2d27692dced98323ccdab8f5
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-10-13 17:44:53 -0300

    Adicionando contato via whatsapp

--- aurora_village/src/pages/main.js
+++ aurora_village/src/pages/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
+import WhatsApp from '../components/WhatsApp';
 
 class Main extends React.Component {
   render() {
     return (
       <main>
         <Header />
+        <WhatsApp />
       </main>
     );
   }
