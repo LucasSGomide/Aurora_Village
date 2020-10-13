262a42809d473c4345e2faf686863d66e2ca8e4f
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-10-13 16:58:58 -0300

    Componente Header

--- aurora_village/src/components/Header.jsx
+++ aurora_village/src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import logo from '../images/HEADER_LOGO.png';
 
 class Header extends React.Component {
   render() {
     return (
-      <nav>
-        Header
-      </nav>
+      <header>
+        <img src={logo} alt="Logomarca Aurora Village" />
+        <nav>
+          <a href="diferenciais">DIFERENCIAIS</a>
+          <a href="video">ASSISTA O VÍDEO</a>
+          <a href="estrutura">ESTRUTURA</a>
+          <a href="localizacao">LOCALIZAÇÃO</a>
+          <a href="contato">CONTATO</a>
+        </nav>
+      </header>
     );
   }
 }
