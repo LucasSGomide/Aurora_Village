7772caae9ec3c37c5bab50fcc3551feb055aaa15
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-10-13 17:26:55 -0300

    Criando section de video

--- aurora_village/src/pages/main.js
+++ aurora_village/src/pages/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
+import VideoSection from '../components/VideoSection';
 
 class Main extends React.Component {
   render() {
     return (
       <main>
         <Header />
+        <VideoSection />
       </main>
     );
   }
