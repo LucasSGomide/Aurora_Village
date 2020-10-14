8a8ce751589bea47419eee6a4e0efbbaa7dbc343
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-10-13 21:35:10 -0300

    Adicionando footer

--- aurora_village/src/pages/main.js
+++ aurora_village/src/pages/main.js
@@ -4,6 +4,7 @@ import Mapa from '../components/Map';
 import WhatsApp from '../components/WhatsApp';
 import VideoSection from '../components/VideoSection';
 import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
 
 class Main extends React.Component {
   render() {
@@ -14,7 +15,7 @@ class Main extends React.Component {
         <VideoSection />
         <ContactSection />
         <Mapa />
-
+        <Footer />
       </main>
     );
   }
